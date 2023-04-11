ace3df40655996f9b88cc30fa6a0e25d2ed46d12
Ákos Takács <takacs.akos@it-sziget.hu>
2023-04-11 23:31:34 +0200

    optimize example 12

--- examples/12-web-css-js/README.md
+++ examples/12-web-css-js/README.md
@@ -1,6 +1,6 @@
 # JS and CSS inside the archive
 
-I am sure, you would like to css and javascripts in a web application. 
+I am sure, you would like to have css and javascript in a web application. 
 Phar archives can contain these files. You just need to create an absolute url to them.
 Frameworks can solve it for you but this example shows you a trick inside the routing method 
 using the real URI and the argument of the routing method (uri inside the phar).
--- examples/12-web-css-js/src/index.php
+++ examples/12-web-css-js/src/index.php
@@ -1,15 +1,12 @@
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Example: CSS and JS</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>/css/default.css" type="text/css">
-
+    <link rel="stylesheet" href="<?= BASE_URL . '/css/default.css'?>" type="text/css">
 </head>
 <body>
-    <div id="time">
-        <?= date('H:i:s') ?>
-    </div>
+    <div id="time" class="hidden"><?= date('H:i:s') ?></div>
 
-    <script src="<?= BASE_URL ?>/js/example.js"></script>
+    <script src="<?= BASE_URL . '/js/example.js'?>"></script>
 </body>
 </html>
